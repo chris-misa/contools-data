12d3eb5cee53c01ebe48a95ae21a5b7d1074d73e
tank <chris@chrismisa.com>
2018-07-29 19:44:04 -0700

    Added ip v6 to latest graphs

--- ping/pings/20180727190349/generateHistograms.r
+++ ping/pings/20180727190349/generateHistograms.r
@@ -3,7 +3,9 @@ intervalsStr <- c(0.2, 0.3, 0.5, 1.0)
 sizesStr <- c(16, 56, 120, 504, 1472)
 
 # Sequence of breaks in histogram
-mBreaks <- seq(0,2,0.01)
+mBinSize <- 0.001
+mBreaks <- seq(0,2,0.001)
+mYLabel <- paste("Count (",mBinSize,") sec bins",sep="")
 
 # X-Boundary for graph
 xBounds <- c(0, 0.5)
@@ -26,7 +28,36 @@ for (i in intervalsStr) {
     heading <- paste("Native vs. Container ipv4 i=", format(i,nsmall=1), " s=", s,sep="")
     yMax <- max(nativeHist$counts,containerHist$counts)
     yBounds <- c(0,yMax)
-    plot(nativeHist$breaks[-1], nativeHist$counts, type="l", main=heading, xlim=xBounds, ylim=yBounds, ylab="Count", xlab="")
+    plot(nativeHist$breaks[-1], nativeHist$counts, type="l", main=heading, xlim=xBounds, ylim=yBounds, ylab=mYLabel, xlab="")
+    lines(containerHist$breaks[-1], containerHist$counts,type="l",col="red", xlim=xBounds, ylim=yBounds)
+    mtext("RTT (sec)",line=2,side=1,adj=1)
+    mtext(paste("Native mean:",nativeMean),line=2,side=1,adj=0)
+    mtext(paste("Container mean:",containerMean),line=3,side=1,adj=0)
+    mtext(paste("Difference:",containerMean-nativeMean),line=4,side=1,adj=0)
+    dev.off()
+  }
+}
+
+
+# Generate histograms for ipv6 tests
+for (i in intervalsStr) {
+  for (s in sizesStr) {
+    # Read in native and container data
+    native <- read.table(paste("native_v6_i", format(i,nsmall=1), "_s", s, ".data",sep=""), sep="\n")
+    container <- read.table(paste("container_v6_i", format(i,nsmall=1), "_s", s, ".data",sep=""), sep="\n")
+
+    # Compute histograms, means
+    nativeHist <- hist(native[,1], plot=F, breaks=mBreaks)
+    nativeMean <- mean(native[,1])
+    containerHist <- hist(container[,1], plot=F, breaks=mBreaks)
+    containerMean <- mean(container[,1])
+
+    # Graph native and container histograms on same axis
+    pdf(file=paste("native_vs_container_v6_i", format(i,nsmall=1), "_s", s, ".pdf",sep=""), width=5, height=5)
+    heading <- paste("Native vs. Container ipv6 i=", format(i,nsmall=1), " s=", s,sep="")
+    yMax <- max(nativeHist$counts,containerHist$counts)
+    yBounds <- c(0,yMax)
+    plot(nativeHist$breaks[-1], nativeHist$counts, type="l", main=heading, xlim=xBounds, ylim=yBounds, ylab=mYLabel, xlab="")
     lines(containerHist$breaks[-1], containerHist$counts,type="l",col="red", xlim=xBounds, ylim=yBounds)
     mtext("RTT (sec)",line=2,side=1,adj=1)
     mtext(paste("Native mean:",nativeMean),line=2,side=1,adj=0)
--- ping/pings/20180727190349/generateMeanDifference.r
+++ ping/pings/20180727190349/generateMeanDifference.r
@@ -2,6 +2,10 @@
 intervalsStr <- c(0.2, 0.3, 0.5, 1.0)
 sizesStr <- c(16, 56, 120, 504, 1472)
 
+#
+# IPv4
+#
+
 # Graph bounds
 xBounds <- range(sizesStr)
 yBounds <- c(0.04,0.1)
@@ -11,8 +15,8 @@ colors <- c("red","blue","green","black")
 colI <- 1
 
 # Set up the plot
-pdf(file="meanDifferences.pdf", width=5, height=5)
-plot(xBounds, yBounds, type="n", main="RTT Mean Differences", ylab="Mean Difference (sec)",xlab="Payload Size (bytes)")
+pdf(file="meanDifferencesV4.pdf", width=5, height=5)
+plot(xBounds, yBounds, type="n", main="RTT Mean Differences (IPV4)", ylab="Mean Difference (sec)",xlab="Payload Size (bytes)")
 
 # Add legend
 legend(0, yBounds[2], legend=intervalsStr, col=colors, lwd=2, title="Ping Interval (sec)")
@@ -37,5 +41,46 @@ for (i in intervalsStr) {
   colI <- colI + 1
 }
 
+dev.off()
+
+
+#
+# IPv6
+#
+
+# Graph bounds
+xBounds <- range(sizesStr)
+yBounds <- c(0.03,0.07)
+
+# Colors
+colors <- c("red","blue","green","black")
+colI <- 1
+
+# Set up the plot
+pdf(file="meanDifferencesV6.pdf", width=5, height=5)
+plot(xBounds, yBounds, type="n", main="RTT Mean Differences (IPV6)", ylab="Mean Difference (sec)",xlab="Payload Size (bytes)")
+
+# Add legend
+legend(0, yBounds[2], legend=intervalsStr, col=colors, lwd=2, title="Ping Interval (sec)")
+
+# Loop through data files
+for (i in intervalsStr) {
+
+  # Create line for this interval
+  differences <- c()
+  for (s in sizesStr) {
+    # Read in native and container data
+    native <- read.table(paste("native_v6_i", format(i,nsmall=1), "_s", s, ".data",sep=""), sep="\n")
+    container <- read.table(paste("container_v6_i", format(i,nsmall=1), "_s", s, ".data",sep=""), sep="\n")
+
+    # Compute means
+    nativeMean <- mean(native[,1])
+    containerMean <- mean(container[,1])
+    differences <- c(differences, containerMean - nativeMean)
+  }
+  # Add line to plot
+  lines(sizesStr, differences, type="l",col=colors[colI])
+  colI <- colI + 1
+}
 
 dev.off()
